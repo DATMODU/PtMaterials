03c1ac04e3727e5d79d58cf63f866da66670286c
3x3x3 <ivmivm001@gmail.com>
2019-02-12 01:24:16 +0900

    중간저장

---
+++ 190209_SWIG_JunHyeong/example3/ex3.cxx
@@ -0,0 +1,15 @@
+// example3/ex3.cxx
+
+#include "simple.h"
+
+long sum_tuple(PyObject* input_tuple) {
+
+}
+
+PyObject* concat_list(PyObject* input_list1, PyObject* input_list2) {
+
+}
+
+PyObject* get_dict_value(PyObject* input_dict, char* key_name) {
+
+}
---
+++ 190209_SWIG_JunHyeong/example3/ex3.i
@@ -0,0 +1,10 @@
+// example3/ex3.i
+
+%module swig_ex3
+
+%{
+#define SWIG_FILE_WITH_INIT
+#include "ex3.h"
+%}
+
+%include "ex3.h"
