74072eab3a27cd530cd7d2b3040f6e4ea2d380f1
3x3x3 <ivmivm001@gmail.com>
2019-02-15 21:30:33 +0900

    그래프 나오게 수정

--- 190209_SWIG_JunHyeong/example5/src/main_ex5_2.py
+++ 190209_SWIG_JunHyeong/example5/src/main_ex5_2.py
@@ -2,34 +2,51 @@
 import numpy as np
 import swig_ex5.core.global_func as gf
 import swig_ex5.core.calc_func as cf
+import matplotlib.pyplot as plt
 
-data_ndarr = np.random.random_sample(10000000)
-data_list = data_ndarr.tolist()
+data_size = []
+using_numpy = []
+c_using_rand_access = []
+c_using_iterator = []
+c_using_ndarray = []
 
-st_time = gf.get_timestamp_16d()
-ret = data_ndarr.std()
-ed_time = gf.get_timestamp_16d()
 
-print(ret)
-print(ed_time - st_time)
+for i in range(1, 100):
+	data_size.append(i * 10000)
 
-st_time = gf.get_timestamp_16d()
-ret = cf.get_std_using_rand_access(data_list)
-ed_time = gf.get_timestamp_16d()
+for n in data_size:
+	data_ndarr = np.random.random_sample(n)
+	data_list = data_ndarr.tolist()
 
-print(ret)
-print(ed_time - st_time)
+	st_time = gf.get_timestamp_16d()
+	ret = data_ndarr.std()
+	ed_time = gf.get_timestamp_16d()
 
-st_time = gf.get_timestamp_16d()
-ret = cf.get_std_using_iterator(data_list)
-ed_time = gf.get_timestamp_16d()
+	using_numpy.append(ed_time - st_time)
 
-print(ret)
-print(ed_time - st_time)
+	st_time = gf.get_timestamp_16d()
+	ret = cf.get_std_using_rand_access(data_list)
+	ed_time = gf.get_timestamp_16d()
 
-st_time = gf.get_timestamp_16d()
-ret = cf.get_std_using_ndarray(data_ndarr)
-ed_time = gf.get_timestamp_16d()
+	c_using_rand_access.append(ed_time - st_time)
 
-print(ret)
-print(ed_time - st_time)
+	st_time = gf.get_timestamp_16d()
+	ret = cf.get_std_using_iterator(data_list)
+	ed_time = gf.get_timestamp_16d()
+
+	c_using_iterator.append(ed_time - st_time)
+
+	st_time = gf.get_timestamp_16d()
+	ret = cf.get_std_using_ndarray(data_ndarr)
+	ed_time = gf.get_timestamp_16d()
+
+	c_using_ndarray.append(ed_time - st_time)
+
+plt.plot(data_size, using_numpy, label="np.std()")
+plt.plot(data_size, c_using_rand_access, label="C++(Input List, Random Access)")
+plt.plot(data_size, c_using_iterator, label="C++(Input List, Iterator)")
+plt.plot(data_size, c_using_ndarray, label="C++(Input np.array, Random Access)")
+plt.legend(loc='upper left')
+plt.xlabel("Data Length")
+plt.ylabel("Time (Micro Seconds)")
+plt.show()
